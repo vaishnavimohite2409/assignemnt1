d5e4dc89522ad3ab0f5da9fa23dc9bce8d6c8b6d
vaishnavimohite2409 <vaishnavimohite364@gmail.com>
2025-04-09 18:50:50 +0530

    Add files via upload

---
+++ assignemt 1 task 1.py
@@ -0,0 +1,27 @@
+# Basic Calculations
+# Calculator Program in Python by using input() and format() functions
+
+#Promting input from the user
+
+a1 = float(input("Enter the First Number: "))
+a2 = float(input("Enter the Second Number: "))
+
+#addition
+
+print("{} + {} = ".format(a1, a2))
+print(a1 + a2)
+
+#subtraction
+
+print("{} - {} = ".format(a1, a2))
+print(a1 - a2)
+
+#multiplication
+
+print("{} * {} = ".format(a1, a2))
+print(a1 * a2)
+
+#division
+
+print("{} / {} = ".format(a1, a2))
+print(a1 / a2)
